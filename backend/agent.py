b596ab17d2713d01d347aaafa1a931001172720f
yimol <yimoliu13196@gmail.com>
2025-10-25 14:26:21 -0400

    Added get suggestions functionality

--- backend/agent.py
+++ backend/agent.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 from google import genai
+import re
 
 class Agent:
     # API Key, remove later
     google_api_key = "AIzaSyAdCvnxTFuXUCBLK3KX6rtzlyto1qaBA_U"
+
+    # Gemini Model
+    model_name = "gemini-2.5-flash"
     def __init__(self, media_type, name: str = ""):
         if media_type == "artwork":
             self.__artwork = name
@@ -13,6 +17,8 @@ class Agent:
             current_directory = current_file.parent
             prompts = current_directory / "Prompts"
             self.__artworks_extracting_themes_prompt = (prompts / "Artworks_Extracting_Themes_Prompt.txt").read_text(
+                encoding="utf-8")
+            self.__artworks_get_suggestions_prompt = (prompts/ "Artworks_Get_Suggestions_Prompt.txt").read_text(
                 encoding="utf-8")
 
         # Load API key from environment variable
@@ -25,15 +31,27 @@ class Agent:
 
     def get_themes(self):
         response = self.__client.models.generate_content(
-            model="gemini-2.5-flash",
+            model=Agent.model_name,
             contents=self.__artworks_extracting_themes_prompt
         )
-        return response
+        return response.text
 
+    def suggestions(self):
+        response = self.__client.models.generate_content(
+            model=Agent.model_name,
+            contents=self.__artworks_get_suggestions_prompt
+        )
+        gemini_list = eval(re.sub(r'```python\n?|```\n?', '', response.text.strip()))
+        suggestion_list = []
+        for innerList in gemini_list:
+            if type(innerList) is list:
+                suggestion_list.append(
+                    {"Name": innerList[0], "Artist": innerList[1], "Year": innerList[2],
+                     "Current Location": innerList[3], "Wikipedia": innerList[4]
+                     })
+        return suggestion_list
 
-'''
-if response.text == "ERROR":
-    print("I do not recognize that artwork. Please try again.")
-    print("")
-else:
-    print(response.text)'''
+my_agent = Agent("artwork", "The Scream")
+my_agent.add_artwork_to_prompt()
+print(my_agent.get_themes())
+print(my_agent.suggestions())
